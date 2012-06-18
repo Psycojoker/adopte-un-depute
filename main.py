387269686b06c7c9a7a88e0f8def9ebc0834538d
Laurent Peuch <cortex@worlddomination.be>
2012-06-18 19:11:46 +0200

    [enh] add wonderfull list of deputies by letters on homepage

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html", deputes=Depute.collection.find(sort=[("nom_de_famille",1)]))
+    deputes_by_letter = {}
+    for depute in Depute.collection.find(sort=[("nom_de_famille",1)]):
+        if not deputes_by_letter.get(depute["nom_de_famille"][0]):
+            deputes_by_letter[depute["nom_de_famille"][0]] = []
+        deputes_by_letter[depute["nom_de_famille"][0]].append(depute)
+    return render_template("home.html", deputes_by_letter=deputes_by_letter, letters=sorted(deputes_by_letter.keys())[1:])
 
 
 @app.route("/depute/<depute>/")
