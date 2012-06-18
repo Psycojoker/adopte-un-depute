9c07816445aa1e2fb7668be557700dc407827ea3
Laurent Peuch <cortex@worlddomination.be>
2012-06-18 23:25:18 +0200

    [fix] update_db was in devel mode

--- update_db.py
+++ update_db.py
@@ -1,10 +1,9 @@
 from json import load
-#from urllib import urlopen
+from urllib import urlopen
 from database import Depute
 
 if __name__ == '__main__':
-    #for depute in load(urlopen("http://www.nosdeputes.fr/deputes/enmandat/json"))["deputes"]:
-    for depute in load(open("deputes.json"))["deputes"]:
+    for depute in load(urlopen("http://www.nosdeputes.fr/deputes/enmandat/json"))["deputes"]:
         dep = depute["depute"]
         if not Depute.collection.find_one({"id": dep["id"]}):
             print "add new dep:", dep["nom"]
