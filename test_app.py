457d94092256bb1be0dba0a681117da6e3432ccc
Laurent Peuch <cortex@worlddomination.be>
2012-06-23 23:16:29 +0200

    [mod] use url_for in tests

--- test_app.py
+++ test_app.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 import unittest
 from test_utils import prepare_db_for_tests, test_depute
+from flask import url_for
 from main import app
 from database import Depute
 
@@ -10,32 +11,37 @@ class FlaskrTestCase(unittest.TestCase):
         prepare_db_for_tests()
         self.app = app.test_client()
         self.depute = Depute(test_depute).save()
+        self.ctx = app.test_request_context()
+        self.ctx.push()
+
+    def tearDown(self):
+        self.ctx.pop()
 
     def test_empty_home(self):
         Depute.collection.remove()
-        rv = self.app.get("/")
+        rv = self.app.get(url_for("home"))
         self.assertTrue("Éric Ciotti" not in rv.data)
 
     def test_deputy_on_home(self):
-        rv = self.app.get("/")
+        rv = self.app.get(url_for("home"))
         self.assertTrue("Éric Ciotti" in rv.data)
 
     def test_deputy_personnal_page(self):
-        rv = self.app.get("/depute/%s/" % self.depute.slug)
+        rv = self.app.get(url_for("depute", depute=self.depute.slug))
         self.assertTrue("Éric Ciotti" in rv.data)
 
     def test_link_to_nosdeputes_fr(self):
-        rv = self.app.get("/depute/%s/" % self.depute.slug)
+        rv = self.app.get(url_for("depute", depute=self.depute.slug))
         self.assertTrue("http://www.nosdeputes.fr/eric-ciotti" in rv.data)
 
     def test_link_to_memopol(self):
-        rv = self.app.get("/depute/%s/" % self.depute.slug)
+        rv = self.app.get(url_for("depute", depute=self.depute.slug))
         self.assertTrue('https://memopol.lqdn.fr/france/assemblee/depute/EricCiotti/' in rv.data)
 
     def test_link_to_an(self):
-        rv = self.app.get("/depute/%s/" % self.depute.slug)
+        rv = self.app.get(url_for("depute", depute=self.depute.slug))
         self.assertTrue("http://www.assembleenationale.fr/13/tribun/fiches_id/330240.asp" in rv.data)
 
     def test_display_group(self):
-        rv = self.app.get("/depute/%s/" % self.depute.slug)
+        rv = self.app.get(url_for("depute", depute=self.depute.slug))
         self.assertTrue(self.depute.groupe_sigle.encode("Utf-8") in rv.data)
